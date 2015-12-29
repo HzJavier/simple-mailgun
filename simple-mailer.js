310915056004f796fe43faa74ba957c459f4c958
HzJavier <javier.hernandez@wizeservices.com>
2015-12-28 21:02:35 -0700

    Refactor module to accept config on creation

--- simple-mailer.js
+++ simple-mailer.js
@@ -1,30 +1,34 @@
 var request = require('request');
 var Q = require('q');
-var GLOBALS = require('./config');
-
-var mailer = {};
-
-mailer.send = function (config) {
-  var deferred = Q.defer();
-
-  request.post(GLOBALS.mailgun, {
-    'auth': {
-      'user': GLOBALS.user,
-      'pass': GLOBALS.pass 
-    },
-    form: {
-      'from': GLOBALS.sender,
-      'to': config.recipient,
-      'subject': config.subject,
-      'text': config.message 
-    }
-  }, function (err, httpResponse, body) {
-    if (err) { deferred.reject(err); };
-
-    deferred.resolve(httpResponse);
-  });
-
-  return deferred.promise;
-};
 
 module.exports = mailer;
+function mailer (GLOBALS) {
+  GLOBALS = GLOBALS || {user: 'api'};
+
+  this.send = function (config) {
+    var deferred = Q.defer();
+
+    request.post('https://api.mailgun.net/v3/' + GLOBALS.domain + '.mailgun.org/messages', {
+      'auth': {
+        'user': GLOBALS.user,
+        'pass': GLOBALS.key
+      },
+      form: {
+        'from': config.sender + '<mailgun@' + GLOBALS.domain + '.mailgun.org>',
+        'to': config.recipient,
+        'subject': config.subject,
+        'text': config.message 
+      }
+    }, function (err, httpResponse, body) {
+      if (err) { deferred.reject(err); };
+
+      deferred.resolve(httpResponse);
+    });
+
+    return deferred.promise;
+  };
+
+  return {
+    send: send
+  };
+}
